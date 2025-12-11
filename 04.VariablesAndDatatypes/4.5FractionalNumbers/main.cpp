c435885122c2b48b1951a82f3ece2849f0867625
AnuragDubey14 <anuragdu125@gmail.com>
2025-12-11 10:37:13 +0530

    integers modifier

--- 04.VariablesAndDatatypes/4.5FractionalNumbers/main.cpp
+++ 04.VariablesAndDatatypes/4.5FractionalNumbers/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+using namespace std;
 
-consteval int get_value(){
-    return 3;
-}
 
 int main(){
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
+    //Declare and initialize the variables
+    float number1 {1.12345678901234567890f};
+    double number2 {1.12345678901234567890};
+    long double number3 {1.12345678901234567890L};
+
+    //Print out the sizes
+    cout<<"Size of Float : "<<sizeof(float)<<endl;
+    cout<<"Size of Double : "<<sizeof(double)<<endl;
+    cout<<"Size of long double : "<<sizeof(long double)<<endl;
+
     return 0;
 }
